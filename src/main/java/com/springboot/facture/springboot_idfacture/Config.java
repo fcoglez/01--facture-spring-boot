dcd064aa95044f01f8037d5d84b85485421a06a9
francisco gonzalez <franciscogonzalezhontoria@gmail.com>
2024-08-23 13:12:42 +0200

    adding annotations @Primary and @Qualifer to Beans

--- src/main/java/com/springboot/facture/springboot_idfacture/Config.java
+++ src/main/java/com/springboot/facture/springboot_idfacture/Config.java
@@ -4,21 +4,35 @@ import com.springboot.facture.springboot_idfacture.models.Item;
 import com.springboot.facture.springboot_idfacture.models.Product;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-@PropertySource("classpath:data.yml")
+@PropertySource(value = "classpath:data.properties", encoding = "UTF-8")
 public class Config {
+
 
     @Bean
     List<Item> itemsFacture(){
         return  Arrays.asList(
-                new Item(
-                        new Product("Camara sonic", 800), 2),
-                        new Item(new Product("Bicicleta monty", 1200), 4)
+                new Item(new Product("Camara sonic", 800), 2),
+                new Item(new Product("Bicicleta monty", 1200), 4)
+        );
+    }
+
+    //@Primary
+    @Bean("default")
+    List<Item> itemsFactureOffice(){
+        return  Arrays.asList(
+                new Item(new Product("Teléfono iPhone", 1000), 2),
+                new Item(new Product("Silla ergonómica", 250), 4),
+                new Item(new Product("Laptop Dell", 1500), 1),
+                new Item(new Product("Escritorio de oficina", 300), 3),
+                new Item(new Product("Monitor Samsung", 500), 2),
+                new Item(new Product("Impresora HP", 200), 5)
         );
     }
 }
--- src/main/java/com/springboot/facture/springboot_idfacture/models/Facture.java
+++ src/main/java/com/springboot/facture/springboot_idfacture/models/Facture.java
@@ -1,6 +1,9 @@
 package com.springboot.facture.springboot_idfacture.models;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +15,28 @@ public class Facture {
     @Autowired
     private Client client;
 
-    @Value("${facture.name}")
+    @Value("${facture.office.name}")
     private String description;
 
     @Autowired
+    @Qualifier("default")
     private List<Item> listItems;
+
+    public Facture() {
+
+    }
+
+    @PostConstruct //Se ejecuta justo después de llamar al constructor
+    public void init(){
+        client.setName("Faku");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        // Aquí cierras cualquier recurso adicional o realizas limpieza
+        System.out.println("Este método se ejecuta justo cuando el " +
+                            "contexto de spring destruye este Bean");
+    }
 
 
     public Client getClient() {
@@ -41,5 +61,15 @@ public class Facture {
 
     public void setListItems(List<Item> listItems) {
         this.listItems = listItems;
+    }
+
+    public String getTotal(){
+//       int total = 0;
+//       for (Item item : items){
+//           total += items
+//       }
+       return listItems.stream()
+               .map(item -> item.getPriceTotal())
+               .reduce(0, (sum, importe) -> sum + importe) + " €";
     }
 }
